70c0eff3da2fe20070315b58ddf39d7820cbaacf
TpainMcain <trevorwp77@gmail.com>
2023-07-18 19:31:45 -0500

    add simple navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,32 @@
-import logo from './logo.svg';
+// Importing necessary React libraries and hooks
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
 import './App.css';
 import MainPage from './components/MainPage';
 import desktopBackground from './images/169.webp'
 import mobileBackground from './images/mobile.webp'
 
 function App() {
-  const imageUrl = window.innerWidth >= 650 ? desktopBackground : mobileBackground;
+  const [imageUrl, setImageUrl] = useState(window.innerWidth >= 650 ? desktopBackground : mobileBackground);
+
+  // Update the image URL on window resize
+  useEffect(() => {
+    const handleResize = () => {
+      setImageUrl(window.innerWidth >= 650 ? desktopBackground : mobileBackground);
+    };
+    
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className="App" style={{backgroundImage: `url(${imageUrl})` }}>
-      <MainPage /> =
+      <Navbar />
+      <MainPage />
     </div>
   );
 }
