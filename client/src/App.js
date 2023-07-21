0fe6f206a59c672c5ab8e6a187527f44cc528c05
TpainMcain <trevorwp77@gmail.com>
2023-07-20 21:06:56 -0500

    add login and register buttons to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import './App.css';
 import MainPage from './components/MainPage';
 import Features from './components/Features';
 import About from './components/About';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
           <Route path="/" element={<MainPage />} />
           <Route path="/features" element={<Features />} />
           <Route path="/about" element={<About />} />
+          {/* <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} /> */}
           <Route path="*" element={<MainPage />} />
         </Routes>
       </div>
