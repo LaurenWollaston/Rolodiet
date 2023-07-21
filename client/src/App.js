983fbd677fa1878fc1e8f89813e59462a9f6785e
TpainMcain <trevorwp77@gmail.com>
2023-07-20 21:08:56 -0500

    add login and register buttons to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 import MainPage from './components/MainPage';
 import Features from './components/Features';
 import About from './components/About';
-import Login from './components/Login';
-import Register from './components/Register';
+// import Login from './components/Login';
+// import Register from './components/Register';
 
 function App() {
   return (
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,3 +1,3 @@
-import React, { useState } from 'react';
-import { gql, useMutation } from '@apollo/client';
-import { useNavigate } from 'react-router-dom';
+// import React, { useState } from 'react';
+// import { gql, useMutation } from '@apollo/client';
+// import { useNavigate } from 'react-router-dom';
