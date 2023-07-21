7feff859e164e4baa3b8ce1ec75d28045803e8a0
jakefair <100549835+jakefair97@users.noreply.github.com>
2023-07-20 19:19:44 -0500

    include authMiddleware and access point to API in server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -34,6 +35,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
 //Create instance of Apollo server with GraphQL schema
 const startApollo = async () => {
