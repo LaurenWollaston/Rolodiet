bad49dd0ba5e079eff75c50a17738cba5f42bc46
Chartok <m.bhimjee@outlook.com>
2023-07-21 16:42:58 -0500

    moved apolloClient.js to utils folder

--- client/src/utils/apolloClient.js
+++ client/src/utils/apolloClient.js
@@ -1,21 +1,27 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import {
+    ApolloClient,
+    InMemoryCache,
+    createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
-const cache = new InMemoryCache();
+const httpLink = createHttpLink({
+    uri: 'http://localhost:3001/graphql'
+});
+
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('token');
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? ` Bearer ${token}`: "",
+        }
+    }
+});
 
 const client = new ApolloClient({
-    // Provide required constructor fields
-    cache: cache,
-    uri: 'http://localhost:3001/',
-
-    // Provide some optional constructor fields
-    name: 'react-web-client',
-    version: '1.3',
-    queryDeduplication: false,
-    defaultOptions: {
-        watchQuery: {
-            fetchPolicy: 'cache-and-network',
-        },
-    },
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
 });
 
 export default client;
