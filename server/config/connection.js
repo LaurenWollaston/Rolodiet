deb0838e4c7762b37793c2f65cd98b53b57fd47a
Chartok <m.bhimjee@outlook.com>
2023-07-23 06:29:17 -0500

    reverted connection.js code to use mongoose instead of mongodb atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,27 +1,8 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.MONGODB_URI || "mongodb+srv://mbhimjeea:4PAMEfMfgmrIBUgD@recipe-search.gkpzti5.mongodb.net/?retryWrites=true&w=majority";
+const mongoose = require('mongoose');
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://mbhimjeea:4PAMEfMfgmrIBUgD@recipe-search.gkpzti5.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
-async function run() {
-    try {
-        // Connect the client to the server (optional starting in v4.7)
-        await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } catch (error) {
-        // Ensures that the client will close when you finish/error
-        console.error('There was an error connecting to MongoDB', error);
-    }
-}
-run();
-
-module.exports = client;
+module.exports = mongoose.connection;
