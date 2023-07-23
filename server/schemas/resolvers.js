77c0a92496433ac4ee2ed84e09366fe8de82fe52
Chartok <m.bhimjee@outlook.com>
2023-07-23 04:12:13 -0500

    Resolved merge conflict in resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 const fetch = require('node-fetch');
 const { User } = require('../models');
-=======
-const { User, Recipe } = require('../models');
->>>>>>> heroku/main
 const signToken = require('../utils/auth');
 
 const resolvers = {
@@ -14,7 +10,6 @@ const resolvers = {
                 $or: [{ _id: id }, { username }],
             });
         },
-<<<<<<< HEAD
 
         me: async (_, __, context) => {
             if (context.user) {
@@ -41,23 +36,6 @@ const resolvers = {
                 link: recipe.url
             }));
         },
-=======
-        findAllRecipes: async (_, { page, perPage }) => {
-            try {
-              const startIndex = (page - 1) * perPage;
-              const endIndex = startIndex + perPage;
-      
-              console.log('Fetching recipes from index:', startIndex, 'to', endIndex);
-        
-              const recipes = await Recipe.find().skip(startIndex).limit(perPage);
-        
-              return recipes;
-            } catch (error) {
-              console.error('Error fetching recipes:', error);
-              throw new Error('Failed to fetch recipes.');
-            }
-          },
->>>>>>> heroku/main
     },
     Mutation: {
         // Create new user with signed webtoken and send to client
