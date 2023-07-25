1760932b0d54e0949b1686c930416d0bb10963ca
Lauren Wollaston <laurenwollaston@outlook.com>
2023-07-25 17:02:45 -0500

    deployment attempt

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/good-eats');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://laurenwollaston:Wd,4J%,8,>yYkZB@cluster0.bby5jkg.mongodb.net/?retryWrites=true&w=majority'|| 'mongodb://127.0.0.1:27017/good-eats');
 
 module.exports = mongoose.connection;
