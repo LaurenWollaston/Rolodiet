582f0e204d0a314fd8380f2455b29c50ace3c4bc
Chartok <m.bhimjee@outlook.com>
2023-07-25 18:10:43 -0500

    removed concat from import

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -1,4 +1,4 @@
-import { ApolloClient, InMemoryCache, createHttpLink, concat } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 // Replace this with your GraphQL server endpoint
