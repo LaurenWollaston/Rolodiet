2b5a537b78ef1776af6529384850257172c17662
Lauren Wollaston <laurenwollaston@outlook.com>
2023-07-25 06:34:46 -0500

    Trying to push one last time before just making a new branch

--- server/populateDb.js
+++ server/populateDb.js
@@ -3,6 +3,26 @@ const Recipe = require('./models/Recipe'); // Adjust the path if needed
 
 // Replace the following with the path to your JSON file
 const jsonFilePath = './recipes.json';
+const Epithymia = './seeds/Epithymia.json';
+const Errinnys = './seeds/Errinnys.json';
+const Kakia = './seeds/Kakia.json';
+const Phthonos = './seeds/Phthonos.json';
+const Zelos = './seeds/Zelos.json';
+
+// Function to insert data into the database
+const insertData = async (data) => {
+  // Check if the 'description' field is missing or empty
+  if (!data.description || data.description.trim() === '') {
+    console.warn('Skipping document without description:', data.title);
+    return;
+  }
+
+  try {
+    await Recipe.create(data);
+  } catch (error) {
+    console.error('Error inserting document:', error);
+  }
+};
 
 // Function to populate the database with data from the JSON file
 const populateDatabase = async () => {
@@ -13,18 +33,46 @@ const populateDatabase = async () => {
       useUnifiedTopology: true,
     });
 
-    // Read the JSON file
+    // Read the JSON files with error handling
     const jsonData = require(jsonFilePath);
+    const EpithymiaData = require(Epithymia);
+    const ErrinnysData = require(Errinnys);
+    const KakiaData = require(Kakia);
+    const PhthonosData = require(Phthonos);
+    const ZelosData = require(Zelos);
 
     // Check if the database is already populated
     const recipeCount = await Recipe.countDocuments();
     if (recipeCount > 0) {
       console.log('Database is already populated. Exiting...');
+      mongoose.connection.close();
       return;
     }
 
     // Insert data into the database
-    await Recipe.insertMany(jsonData);
+    for (const data of jsonData) {
+      await insertData(data);
+    }
+
+    for (const data of EpithymiaData) {
+      await insertData(data);
+    }
+
+    for (const data of ErrinnysData) {
+      await insertData(data);
+    }
+
+    for (const data of KakiaData) {
+      await insertData(data);
+    }
+
+    for (const data of PhthonosData) {
+      await insertData(data);
+    }
+
+    for (const data of ZelosData) {
+      await insertData(data);
+    }
 
     console.log('Database populated successfully!');
   } catch (error) {
