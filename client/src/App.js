550511ebc7a193ef6aab736befeabd348ecc7367
Chartok <m.bhimjee@outlook.com>
2023-07-28 02:34:20 -0500

    renamed LoginForm

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 import MainPage from './pages/MainPage';
 import Features from './pages/Features';
 import About from './pages/About';
-import LoginForm from './pages/LoginForm';
-import Register from './pages/';
+import Login from './pages/login';
+import Register from './pages/register';
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
             <Route path="/" element={<MainPage />} />
             <Route path="/features" element={<Features />} />
             <Route path="/about" element={<About />} />
-            <Route path="/login" element={<LoginForm />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="*" element={<MainPage />} />
           </Routes>
