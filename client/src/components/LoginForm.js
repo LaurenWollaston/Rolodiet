88335497f1a497e81295ff90c080cfddff5f7040
Chartok <m.bhimjee@outlook.com>
2023-07-28 01:54:11 -0500

    refactored login form to simplify and clean up

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,8 +2,8 @@ import { useContext, useState } from "react";
 import { AuthContext } from "../context/authContext";
 import { useForm } from "../utils/hooks";
 import { useMutation } from '@apollo/react-hooks';
-import { Form, Button, Alert } from 'react-bootstrap';
-import './LoginForm.css'; // Import the CSS file
+
+import { Button, TextField, Container, Stack, Alert } from '@mui/material';
 
 import { gql } from 'graphql-tag';
 import { useNavigate } from "react-router-dom";
@@ -37,8 +37,8 @@ const LoginForm = () => {
         password: ''
     });
 
-    const [loginUser, { loading, error }] = useMutation(LOGIN_USER, {
-        update(proxy, { data: { loginUser: userData } }) {
+    const [loginUser] = useMutation(LOGIN_USER, {
+        update(_, { data: { loginUser: userData } }) {
             context.login(userData);
             navigate('/');
         },
@@ -49,50 +49,29 @@ const LoginForm = () => {
     });
 
     return (
-        <>
-            <Form noValidate validated={validated} onSubmit={handleFormSubmit} className='loginForm'>
-                <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger' className='loginFormAlert'>
-                    Something went wrong with your login credentials!
-                </Alert>
-
-                <Form.Group className='mb-3 loginFormInputGroup'>
-                    <Form.Label htmlFor='email' className='loginFormLabel'>Email</Form.Label>
-                    <Form.Control
-                        type='text'
-                        placeholder='Your email'
-                        name='email'
-                        onChange={handleInputChange}
-                        value={userFormData.email}
-                        required
-                        className='loginFormControl'
-                        />
-                    <Form.Control.Feedback type='invalid' className='loginFormInvalidFeedback'>Email is required!</Form.Control.Feedback>
-                </Form.Group>
-
-                <Form.Group className='mb-3 loginFormInputGroup'>
-                    <Form.Label htmlFor='password' className='loginFormLabel'>Password</Form.Label>
-                    <Form.Control
-                        type='password'
-                        placeholder='Your password'
-                        name='password'
-                        onChange={handleInputChange}
-                        value={userFormData.password}
-                        required
-                        className='loginFormControl'
-                    />
-                    <Form.Control.Feedback type='invalid' className='loginFormInvalidFeedback'>Password is required!</Form.Control.Feedback>
-                </Form.Group>
-
-                <Button
-                    disabled={!(userFormData.email && userFormData.password)}
-                    type='submit'
-                    variant='success'
-                    className='loginFormButton'>
-                    Submit
-                </Button>
-            </Form>
-        </>
+        <Container spacing={2} maxWidth="sm">
+            <h3>Login</h3>
+            <p>Login to your account</p>
+            <Stack spacing={2} paddingBottom={2}>
+                <TextField
+                    label="Email"
+                    name="email"
+                    type="email"
+                    onChange={onChange}
+                />
+                <TextField
+                    label="Password"
+                    name="password"
+                    type="password"
+                    onChange={onChange}
+                />
+            </Stack>
+            {errors.map(error => (
+                <Alert severity="error">{error.message}</Alert>
+            ))}
+            <Button variant="contained" onClick={onSubmit}>Login</Button>
+        </Container>
     );
-};
+}
 
 export default LoginForm;
